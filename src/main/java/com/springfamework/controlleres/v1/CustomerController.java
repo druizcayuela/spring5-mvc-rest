28ec5841d87c58c89fbc7570a568d120f6b31887
Daniel Ruiz Cayuela <druiz@financialforce.com>
2020-05-03 14:47:26 +0200

    Optimize imports

--- src/main/java/com/springfamework/controlleres/v1/CustomerController.java
+++ src/main/java/com/springfamework/controlleres/v1/CustomerController.java
@@ -4,7 +4,6 @@ import com.springfamework.api.v1.model.CustomerDTO;
 import com.springfamework.api.v1.model.CustomerListDTO;
 import com.springfamework.services.CustomerService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
